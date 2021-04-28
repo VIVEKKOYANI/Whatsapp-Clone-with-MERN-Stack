8a41445cc1995314120a24cbcd29a53eac146aa9
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2021-04-28 19:30:21 +0530

    add components

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,57 @@
+import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
 import './Chat.css'
+import {AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons'
+import {InsertEmoticon, Mic } from '@material-ui/icons'; 
+
 function Chat() {
     return (
-        <div>
-            
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                      <SearchOutlined />  
+                    </IconButton>
+                    <IconButton>
+                      <AttachFile />  
+                    </IconButton>
+                    <IconButton>
+                      <MoreVert />  
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Sonny</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">Sonny</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">Send a message</button>
+                </form>
+                <Mic />
+            </div>
         </div>
     )
 }
