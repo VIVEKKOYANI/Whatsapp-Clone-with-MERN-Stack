2713cbfb8f5bda6ab92eab22428efcaaee689582
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2021-04-30 10:17:14 +0530

    add db connection

--- Beck/server.js
+++ Beck/server.js
@@ -1,5 +1,6 @@
 //importing
 import express from 'express';
+import mongoose  from 'mongoose';
 
 //app config
 const app = express();
@@ -7,7 +8,13 @@ const port = process.env.PORT || 9000;
 //middleware
 
 //DB Config
+const connection_url = 'mongodb+srv://admin:7zn8FsfPmoer2YO3@cluster0.erm5t.mongodb.net/whatsappdb?retryWrites=true&w=majority'
 
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 // ???
 
 //api routes
